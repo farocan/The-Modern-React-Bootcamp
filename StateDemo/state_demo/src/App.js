517b50ae36991343c448ae48a5de24ced0e72342
farocan <anna.fr.1990@gmail.com>
2023-12-27 09:04:44 +0100

    adding Demo component

--- StateDemo/state_demo/src/App.js
+++ StateDemo/state_demo/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Game from './Game';
+import Demo from './Demo';
 import './App.css';
 
 class App extends Component {
   render(){
     return <div className="App">
-      <Game />
+      <Game color="purple" />
+      <Demo animal="Bobcat" food="Pineapple"/>
     </div>;
   }
 }
