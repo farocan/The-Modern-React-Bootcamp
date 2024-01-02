7c2c20607a920c89b92c4c641cd34451a0095389
farocan <anna.fr.1990@gmail.com>
2024-01-02 08:51:40 +0100

    creating lotto app with Ball component

---
+++ StateDemo/lotto/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Ball from './Ball';
+
+function App() {
+  return (
+    <div className="App">
+      <Ball num={17} />
+      <Ball num={13} />
+      <Ball num={11} />
+      <Ball num={9} />
+    </div>
+  );
+}
+
+export default App;
