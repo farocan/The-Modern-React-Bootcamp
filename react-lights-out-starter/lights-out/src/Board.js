114d84d62d06696f4d61d0ce26c3eaaa01f3d428
farocan <anna.fr.1990@gmail.com>
2024-01-11 08:34:16 +0100

    adding styles to Lights Out game

--- react-lights-out-starter/lights-out/src/Board.js
+++ react-lights-out-starter/lights-out/src/Board.js
@@ -98,12 +98,15 @@ class Board extends Component {
 
   render() {
     if(this.state.hasWon){
-      return <h1>YOU WON!!!</h1>;
+      return (
+        <div className='Board-title'>
+          <div className='winner'>
+            <span className='neon-orange'>YOU</span>
+            <span className='neon-blue'>WIN!</span>
+          </div>
+        </div>
+      )
     }
-    // if the game is won, just show a winning msg & render nothing else
-    // TODO
-    // make table board
-    // TODO
     let tblBoard = [];
     for(let y=0; y < this.props.nrows; y++){
       let row = [];
@@ -119,9 +122,16 @@ class Board extends Component {
       tblBoard.push(<tr key={y}>{row}</tr>);
     }
     return (
-      <table className="Board">
-        <tbody>{tblBoard}</tbody>
-      </table>
+      <div>
+        <div className='Board-title'>
+          <div className='neon-orange'>Lights</div>
+          <div className='neon-blue'>Out</div>
+        </div>
+            <table className="Board">
+            <tbody>{tblBoard}</tbody>
+        </table>
+      </div>
+      
     )
   }
 }
