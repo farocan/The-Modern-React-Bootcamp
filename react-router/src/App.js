a3a12a1e9d01ccaf71e2f015c3864b9fd90735b7
farocan <anna.fr.1990@gmail.com>
2024-01-31 09:14:38 +0100

    adding NavLinks

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Routes, Route, Link, Switch, NavLink } from "react-router-dom";
+import { Routes, Route, Switch, NavLink } from "react-router-dom";
 import About from "./About";
 import Dog from "./Dog";
 import Contact from "./Contact";
@@ -12,12 +12,9 @@ class App extends Component {
     return (
       <div className='App'>
         <nav className='App-nav'>
-          <Link to='/'>About</Link>
-          <Link to='/dog'>Dog</Link>
-          <Link to='/contact'>Contact</Link>
-          <a href='/dog'>Dog</a>
-          <a href='/contact'>Contact</a>
-          <a href='/'>About</a>
+          <NavLink exact activeClassName='active-link' to='/'>About</NavLink>
+          <NavLink exact activeClassName='active-link' to='/dog'>Dog</NavLink>
+          <NavLink exact activeClassName='active-link' to='/contact'>Contact</NavLink>
         </nav>
         <Routes>
           <Route path='/' Component={About}/>
