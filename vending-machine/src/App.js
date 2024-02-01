9e2642f32f65ee500b5fa6f205c3e65f51ccdfe1
farocan <anna.fr.1990@gmail.com>
2024-02-01 08:12:36 +0100

    creating vending machine app

---
+++ vending-machine/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Chips from "./Chips";
+import Sardines from "./Sardines";
+import Soda from "./Soda";
+import './App.css';
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <VendingMachine />
+        <Chips />
+        <Sardines />
+        <Soda />
+      </div>
+    );
+  }
+}
+
+export default App;
