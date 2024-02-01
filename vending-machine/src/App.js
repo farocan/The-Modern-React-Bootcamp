e6bc0ce31500d3bcb779bf8dd021252b2788ffdc
farocan <anna.fr.1990@gmail.com>
2024-02-01 08:34:02 +0100

    adding the vending machine routes

--- vending-machine/src/App.js
+++ vending-machine/src/App.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 import Chips from "./Chips";
 import Sardines from "./Sardines";
 import Soda from "./Soda";
+import VendingMachine from "./VendingMachine";
+import {Route, Routes} from "react-router-dom";
 import './App.css';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <VendingMachine />
-        <Chips />
-        <Sardines />
-        <Soda />
+        <Routes>
+          <Route exact path="/" Component={() => <VendingMachine/>}/>
+          <Route exact path="/chips" Component={() => <Chips/>}/>
+          <Route exact path="/sardines" Component={() => <Sardines/>}/>
+          <Route exact path="/soda" Component={() => <Soda/>}/>
+        </Routes>
       </div>
     );
   }
